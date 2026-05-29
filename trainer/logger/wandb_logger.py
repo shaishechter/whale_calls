123567be688bb0762a99a54eb77b8cfb38a5da9a
shaishechter <shais1985@gmail.com>
2026-05-29 07:20:50 +0300

    wandb local logger

--- trainer/logger/wandb_logger.py
+++ trainer/logger/wandb_logger.py
@@ -5,7 +5,6 @@ from typing import Optional
 
 import pandas as pd
 from matplotlib import pyplot as plt
-import wandb
 from lightning.pytorch.loggers import WandbLogger
 import logging
 
@@ -22,9 +21,9 @@ class LocalWandbLogger(WandbLogger):
         self,
         project: str,
         local: bool,
-        save_dir: str,
-        mode: str,
         name: str,
+        save_dir: Optional[os.PathLike] = None,
+        offline: bool = False,
         *args,
         **kwargs,
     ):
@@ -32,48 +31,21 @@ class LocalWandbLogger(WandbLogger):
         Initialize the logger.
         :param local: bool, whether to save the logs locally or upload them to Weights & Biases
         :param save_dir: str, the directory to save the logs to (and other reproducibility info, even if local=False)
+        :param offline: bool, whether to run in offline mode
+        :param name: str, the name of the experiment
         :param args: arguments to pass to the Wand
         """
-        if mode not in ["debug", "async"]:
-            raise ValueError(f"Invalid mode: {mode}, should be 'debug' or 'async'")
-        self._offline = mode == "debug"
 
         self.logs_dir = save_dir
-        self._name = name
-        self._idx = self._get_exp_idx()
         self.local = local
+        self.datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         super().__init__(
             project=project,
             *args,
-            offline=self._offline,
-            name=f"{self._name}-{self._idx}",
+            offline=offline,
+            name=name,
             **kwargs,
         )
-
-    def _get_exp_idx(self) -> int:
-        import sqlite3
-
-        db_path = Path(self.logs_dir) / "idx.db"
-        with sqlite3.connect(str(db_path)) as conn:
-            cursor = conn.cursor()
-            cursor.execute("""CREATE TABLE IF NOT EXISTS run_idx 
-                                (name TEXT PRIMARY KEY, idx INTEGER)""")
-
-            cursor.execute(
-                """
-                    INSERT INTO run_idx (name, idx) 
-                    VALUES (?, 1)
-                    ON CONFLICT(name) 
-                    DO UPDATE SET idx = idx + 1
-                """,
-                (self.name,),
-            )
-
-            idx = cursor.execute(
-                "SELECT idx FROM run_idx WHERE name = ?", (self.name,)
-            ).fetchone()[0]
-            conn.commit()
-        return idx
 
     @property
     def save_dir(self) -> Optional[str]:
@@ -84,12 +56,9 @@ class LocalWandbLogger(WandbLogger):
         return str(
             Path(self.logs_dir)
             / self.experiment.project
-            / "/".join(self.experiment.name.split("-"))
+            / self.experiment.name
+            / f"{self.datetime}_{self.experiment.id}"
         )
-
-    @property
-    def name(self) -> str:
-        return self._name
 
     def log_figure(self, figure: plt.Figure, name: str, epoch: Optional[int] = None):
         """
@@ -130,12 +99,3 @@ class LocalWandbLogger(WandbLogger):
             saving_path = saving_path / f"{epoch}.png"
 
         figure.savefig(saving_path, dpi=400)
-
-    def finalize(self, status: str) -> None:
-        """
-        Finalize the logger, and save the reproducibility info.
-        :param status: str, the status of the training
-        :return:
-        """
-        super().finalize(status)
-        wandb.finish()
